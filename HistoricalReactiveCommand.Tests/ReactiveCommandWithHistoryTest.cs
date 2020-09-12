5a8cb377c7b1f894252e5042df7afad0a85be0ef
GMIKE <roman.soloweow333@ya.ru>
2020-09-13 01:03:18 +0300

    CanExecuteChangeOnExecutingCommandAsync

--- HistoricalReactiveCommand.Tests/ReactiveCommandWithHistoryTest.cs
+++ HistoricalReactiveCommand.Tests/ReactiveCommandWithHistoryTest.cs
@@ -10,6 +10,9 @@ using Xunit;
 using System.Threading;
 using DynamicData.Annotations;
 using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Reactive.Threading.Tasks;
 
 namespace HistoricalReactiveCommand.Tests
 {
@@ -60,6 +63,27 @@ namespace HistoricalReactiveCommand.Tests
             Assert.Equal(25, myNumber);
             command.History.Redo.Execute().Subscribe();
             Assert.Equal(50, myNumber);
+        }
+
+        [Fact]
+        public async Task CanExecuteChangeOnExecutingCommandAsync()
+        {
+
+            ICommand fixture = ReactiveCommandEx.CreateWithHistoryFromObservable<Unit, Unit>(CommandKey,
+              (parameter, result) => Observables.Unit,
+              (parameter, result) => Observables.Unit,
+              _canExecuteSubject,
+              _scheduler);
+
+            List<bool> canExecuteChanged = new List<bool>();
+            fixture.CanExecuteChanged += (s, e) => canExecuteChanged.Add(fixture.CanExecute(null));
+            var myTask = _canExecuteSubject.FirstAsync().ToTask();
+            _canExecuteSubject.OnNext(true);
+            Assert.Equal(1, canExecuteChanged.Count);
+            var b = await myTask;
+            _canExecuteSubject.OnCompleted();
+            Assert.Equal(2, canExecuteChanged.Count);    
+
         }
     }
 }
