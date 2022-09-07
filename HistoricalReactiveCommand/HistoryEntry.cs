07439db91bb2f61a4389a885942f835f00f6f6e7
Kibnet Philosoff <kibnet@hotmail.com>
2022-09-07 11:23:33 +0300

    feat: Добавлено время создание в HistoryEntry

--- HistoricalReactiveCommand/HistoryEntry.cs
+++ HistoricalReactiveCommand/HistoryEntry.cs
@@ -1,4 +1,7 @@
-﻿namespace HistoricalReactiveCommand
+﻿using System;
+using System.Data;
+
+namespace HistoricalReactiveCommand
 {
     public class HistoryEntry
     {
@@ -7,11 +10,13 @@
             Parameter = parameter;
             Result = result;
             CommandKey = commandKey;
+            CreationtTime = DateTime.UtcNow;
         }
         
         public object Parameter { get; set; }
         public object Result { get; set; }
         public string CommandKey { get; set; }
+        public DateTime CreationtTime { get; }
     }
 }
 
